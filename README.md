008bde960181ce0543c45c468e2f49c42e099002
Azeem Zahid <connectingazeem@gmail.com>
2024-05-31 16:18:02 +0500

    CSV GENERATE FILE FROM OLD FILE WITH NEW COLUMNS

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center"><a href="https://php.net" target="_blank"><img src="https://www.php.net/images/logos/php-logo.svg" height="300" alt="PHP LOGO">
+
+## About This Mini Project Generate CSV File.
+
+This tiny project reads the csv file and remove the duplicates entries and generates the new csv file with new columns like:
+
+- First Order Date
+- Last Order Date
+- Days Difference
+- Total Order
+- Quantity
+
+### Created By **Azeem Zahid**
+
+(mailto:connectingazeem@gmail.com).
+
+Date created: 31 May 2024 at 4:16 PM
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+$old_csv_file = "backend-task.csv";
+$new_csv_file  = uniqid() . '-' . "new-task.csv";
+
+$filter_data = [];
+// open the old file first
+$f = fopen($old_csv_file, "r");
+if ($f !== false) {
+    fgetcsv($f, 1000, ",");
+    while (($data = fgetcsv($f, 1000, ",")) !== false) {
+        $orderDate = $data[0];
+        $email = $data[2];
+        $quantity = $data[3];
+
+        if (!isset($filter_data[$email])) {
+            $filter_data[$email]  = [
+                'firstOrderDate' => $orderDate,
+                'lastOrderDate' => $orderDate,
+                'orderDates' => [$orderDate],
+                'totalOrder' => 1,
+                'totalQuantity' => $quantity
+            ];
+        } else {
+            $filter_data[$email]['lastOrderDate'] = $orderDate;
+            $filter_data[$email]['orderDates'][] = $orderDate;
+            $filter_data[$email]['totalOrder']++;
+            $filter_data[$email]['totalQuantity'] = $quantity;
+        }
+    }
+}
+fclose($f);
+
+$newOpen = fopen($new_csv_file, "w");
+if ($newOpen !== false) {
+    // add the headings
+    fputcsv($newOpen, ['First Order Date', 'Last Order Date', 'Days Difference', 'email', 'quantity']);
+
+    foreach ($filter_data as $email => $customer) {
+        $firstOrderDate = $customer['firstOrderDate'];
+        $lastOrderDate = $customer['lastOrderDate'];
+        $daysDifference = strtotime($firstOrderDate) - strtotime($lastOrderDate);
+        $daysDifference = floor($daysDifference / 24 * 60 * 60);
+        $totalOrder = $customer['totalOrder'];
+        $quantity = $customer['totalQuantity'];
+
+        fputcsv($newOpen, [$firstOrderDate, $lastOrderDate, $daysDifference, $totalOrder, $quantity]);
+    }
+}
+fclose($newOpen);
+
+echo "File has been created successfully";
